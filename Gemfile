947b8f1343e50c1b7bfcacd847b6092bfcfd24ad
Naoki Kanazawa <info@designless.net>
2014-11-22 12:30:14 +0900

    Add middleman-imageoptim

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem "middleman"
 
 gem 'middleman-pry'
 
+gem "middleman-imageoptim", "~> 0.1.3"
+
 gem 'slim'
 
 gem 'puma'
@@ -14,16 +16,10 @@ gem 'rake'
 
 gem "middleman-minify-html"
 
-# gem "middleman-navigation"
-# gem "simple-navigation"
-
-# Live-reloading plugin
 gem "middleman-livereload"
 
-# For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 
-# Cross-templating language block fix for Ruby 1.8
 platforms :mri_18 do
   gem "ruby18_source_location"
 end
--- config.rb
+++ config.rb
@@ -107,19 +107,45 @@ set :images_dir, 'assets/images'
 
 activate :livereload
 
-
-# middleman-navigation
-# activate :navigation
+require "uglifier"
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
+
+  set :relative_links, true
+
   activate :minify_css
 
-  # Minify Javascript on build
-  activate :minify_javascript
+  activate :minify_javascript, :compressor => Uglifier.new({
+    :output => {
+      :comments => :all
+    }
+  })
 
   activate :minify_html
+
+  activate :imageoptim do |options|
+    # print out skipped images
+    # options.verbose = true
+
+    # Setting these to true or nil will let options determine them (recommended)
+    # options.nice = true
+    # options.threads = true
+
+    # Image extensions to attempt to compress
+    # options.image_extensions = %w(.png .jpg .gif)
+
+    # compressor worker options, individual optimisers can be disabled by passing
+    # false instead of a hash
+    # options.pngcrush_options  = {:chunks => ['alla'], :fix => false, :brute => false}
+    #options.pngout_options    = {:copy_chunks => false, :strategy => 0}
+    # options.optipng_options   = {:level => 6, :interlace => false}
+    options.pngout_options = false
+    # options.advpng_options    = {:level => 4}
+    # options.jpegoptim_options = {:strip => ['all'], :max_quality => 100}
+    # options.jpegtran_options  = {:copy_chunks => false, :progressive => true, :jpegrescan => true}
+    # options.gifsicle_options  = {:interlace => false}
+  end
 
   # Enable cache buster
   # activate :asset_hash
@@ -130,3 +156,4 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
